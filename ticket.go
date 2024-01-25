324d1c4279e3f3b112f2b2f4fcb0695e8b3476cf
dropthemasquerade <49177445+dropthemasquerade@users.noreply.github.com>
2024-01-25 23:55:55 +0800

    feat: update namespace

--- ticket.go
+++ ticket.go
@@ -14,10 +14,10 @@ const (
 
 // QueueTicketNumber 获取排队号自动增加
 // 每天自动过期
-func QueueTicketNumber(ctx context.Context, ticketPrefix string) string {
+func QueueTicketNumber(ctx context.Context, ticketPrefix string, namespace string) string {
 	// 获取当前日期作为 Redis Key
 	key := time.Now().Format("2006-01-02")
-	fullKey := fmt.Sprintf("%s:%s", prefix, key)
+	fullKey := fmt.Sprintf("%s:%s:%s", prefix, namespace, key)
 
 	// 获取当前排队号
 	currentNumber, err := db.RDB.Incr(ctx, fullKey).Result()
