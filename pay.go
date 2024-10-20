f9dc3c49317daa680d37e3f12ab91ba48ff966eb
dropthemasquerade <49177445+dropthemasquerade@users.noreply.github.com>
2024-10-21 03:12:45 +0800

    add prefix

--- pay.go
+++ pay.go
@@ -2,6 +2,7 @@ package r2id
 
 import (
 	"crypto/rand"
+	"fmt"
 	"math/big"
 	"strings"
 	"time"
@@ -18,15 +19,32 @@ func GenerateRandomString(length int) string {
 	return result.String()
 }
 
-// GenerateOutTradeNo 生成商户订单号
-func GenerateOutTradeNo() string {
-	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_-|*"
+func GenerateOutTradeNo(prefix ...string) string {
+	const charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 	var result strings.Builder
-	//timestamp := time.Now().UnixNano() / int64(time.Millisecond) // 获取当前时间戳（毫秒）
-	result.WriteString(time.Now().Format("20060102150405")) // 使用时间作为订单号的一部分
+
+	// 如果用户提供了前缀，使用第一个前缀，否则默认前缀为 "A"
+	if len(prefix) > 0 {
+		result.WriteString(prefix[0])
+	} else {
+		result.WriteString("A")
+	}
+
+	loc, err := time.LoadLocation("Asia/Shanghai")
+	if err != nil {
+		fmt.Println("Error loading location:", err)
+		return ""
+	}
+
+	// 获取当前时间，并使用时间作为订单号的一部分
+	dt := time.Now().In(loc)
+	result.WriteString(dt.Format("20060102150405"))
+
+	// 随机生成订单号的剩余部分，确保总长度为 32
 	for i := 0; i < 32-len(result.String()); i++ {
 		n, _ := rand.Int(rand.Reader, big.NewInt(int64(len(charset))))
 		result.WriteByte(charset[n.Int64()])
 	}
+
 	return result.String()
 }
